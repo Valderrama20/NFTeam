47526da301375f177cae9815ddf664d17f816918
Valderrama20 <jose23122009@gmai.com>
2023-03-31 13:10:34 -0300

    login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,24 +1,34 @@
+import { useState } from 'react';
 import style from './Login.module.css';
 
 type LoginType = {
   isOpen: boolean;
 };
 
+
+ 
 const Login = ({ isOpen }: LoginType) => {
+
+    const [dataUser, setDataUser] = useState({fullName:"", email:""})
+
+const setData = (data: React.ChangeEvent<HTMLInputElement>) => {
+    setDataUser({...dataUser,[data.target.name]:data.target.value})
+}
   return isOpen ? (
     <form action="" className={style.form}>
+        <label htmlFor="">register</label>
       <div>
         <div className={style.input}>
           <label htmlFor="">Full Name</label>
-          <input type="text" />
+          <input type="text" name="fullName" onChange={setData}/>
         </div>   
         <div className={style.input}>
           <label htmlFor="">Email</label>
-          <input type="text" />
+          <input type="text" name="email" onChange={setData}/>
         </div>
       </div>
-      <div>
-        <input type="submit" />
+      <div >
+        <input type="submit" className={style.btn} />
       </div>
     </form>
   ) : null;
