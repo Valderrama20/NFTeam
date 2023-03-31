bf8023a39f73d26a3dee1a1f8ef0d4ffd3ea7a74
Nico Limo <nicolas.limonoff@gmail.com>
2023-03-31 10:32:22 -0300

    added prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react'
-import './styles/globals.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react';
+import './styles/globals.css';
 
-const activeChain = ChainId.Polygon
+const activeChain = ChainId.Polygon;
 
-const container = document.getElementById('root')
-const root = createRoot(container!)
+const container = document.getElementById('root');
+const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <ThirdwebProvider activeChain={activeChain}>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
-)
+);
