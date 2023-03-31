e164950ea19f0ccb77aeb0f101fb2609abc47be6
Nico Limo <nicolas.limonoff@gmail.com>
2023-03-31 12:57:15 -0300

    added wallet connect logic

---
+++ src/hooks/useWallet.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { useDisconnect, useMetamask, useAddress } from '@thirdweb-dev/react';
+
+const useWallet = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const disconnect = useDisconnect();
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+  const shortAddress = address
+    ? `${address.slice(0, 4)}...${address.slice(-4)}`
+    : '';
+  const isConnected = address ? true : false;
+
+  const handleConnect = async () => {
+    try {
+      setIsLoading(true);
+
+      if (address) {
+        await disconnect();
+      } else {
+        await connectWithMetamask();
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      throw error;
+    }
+  };
+
+  return { handleConnect, shortAddress, address, isLoading, isConnected };
+};
+
+export default useWallet;
