0f25534a72e044595931595488079aad4d27e3d8
Valderrama20 <jose23122009@gmai.com>
2023-04-02 13:07:51 -0300

    Creando landing

--- src/components/infoCard/InfoCard.tsx
+++ src/components/infoCard/InfoCard.tsx
@@ -4,7 +4,7 @@ import style from "./InfoCard.module.css"
 const InfoCard = ({data}: any) => {
     console.log(data)
     return <div className={style.card}> 
-        <div className={style.stickers}></div>
+        <div className={style.png}><img src={data.png} alt="" /></div>
         <div className={style.title}>{data.title}</div>
         <div className={style.text}>{data.text}</div>
     </div>
